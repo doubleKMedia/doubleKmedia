b5cd74fabb2cbf3409f0e9f0aa06e1a3ba528e52
origin <chldlsrb1000@naver.com>
2021-12-25 00:39:50 +0900

    의존성 추가에 따른 App 변경 - recoil, react-router-dom 기본세팅 - index css 최상단으로 위치변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import Main from './components/pages/Main/Main';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <RecoilRoot>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
+    </RecoilRoot>
+  );
 }
 
 export default App;
